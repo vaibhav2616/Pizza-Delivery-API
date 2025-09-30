2c126bb705f026450e6261ee34e5d286da60ede6
vaibhav2616 <rastogiv215@gmail.com>
2025-09-30 12:21:07 +0530

    Initial Commit

---
+++ database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+ 
+engine=create_engine('postgresql://postgres:Vaibhav%40123i@localhost/pizza_delivery'
+    ,echo=True)
+Base =declarative_base()
+session=sessionmaker()
